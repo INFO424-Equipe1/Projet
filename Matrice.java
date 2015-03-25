71ce35b3891e0e70e8f8e1d270cfb42c3cda3b8b
AntoineS73 <antoinesteyer@gmail.com>
2015-03-25 16:18:37 +0100

    Ajout de l'analyse du rectangle pour une image en pgm

--- Matrice.java
+++ Matrice.java
@@ -64,13 +64,15 @@ public class Matrice {
 	return this.nuances;
     }
     
-    public static void analyse_pgm () {
+    public void analyse_pgm () {
 	/* Procedure qui analyse l'image .pgm 
 	 * Affiche les résultats de l'analyse
 	 */ 
 
-	if (this.nuances == 1) {
+        if (this.nuances == 1) {
 	    // Si on a une image seulement avec du noir et du blanc
+	    
+
 	    
 	    int l = 0; // Compteur de la largeur de la figure sur une ligne
 	    int l_max = 0; // Valeur maximale de la largeur de la figure
@@ -78,38 +80,67 @@ public class Matrice {
 	    // coordonnées du point d'origine de la figure
 	    int x=0 ;
 	    int y=0 ; 
+	    
+	    // Booleens mis à jour si on reconnait la figure
+	    boolean estRectangle = false;
+	    boolean estCercle = false;
+	    boolean estTriangle = false;
+
 	    // On parcourt notre image
 	    for (int i=0; i<this.hauteur ; i++) {
        
 		for (int j=0; j<this.largeur ; j++) {
-		    if (this.image[i][j] == 0) {
+    		    if (this.image[i][j] == 0) {
+			// Si on a un pixel noir
 			l++ ;
-			if (x=0 && y=0) {
-			    // on regarde le premier point de la figure
-			    x = j;
-			    y = i;
+    			if (x==0 && y==0) {
+    			    // on regarde le premier point de la figure
+    			    x = j;
+    			    y = i;
+    			}
+			if (j == this.largeur-1 || this.image[i][j+1] == 1) {
+			    // Si on arrive à la fin de la figure
+			    // sur la ligne courante
+			    // on incrémente la hauteur de cette figure
+			    h++;
 			}
-			
-						
-		    }
-		    	    
-	    	}
-
-		if (l > l_max) {
-		    l_max = l ;
+		    }   
+        	}
+		if (l == l_max) {
+		    // Si on a deux lignes de même longueur qui se suivent
+		    // on a un rectangle
+		    estRectangle = true;
 		}
-		
-		l = 0 ;
+		if (l > l_max) {
+		    // On met à jour la ligne maximale
+    		    l_max = l ;
+    		}
+	       
+    		// On arrive à une nouvelle ligne
+    		l = 0 ;
+    	    }
+	    
+	    if (estRectangle) {
+		// Si on a un rectangle
+		if (l_max == h) {
+		    // Le cas d'un carré
+		    System.out.println("La figure est un carré");
+		}
+		else {
+		    System.out.println("La figure est un rectangle");
+		}
+		System.out.println("Point origine : ("+x+","+y+")");
+		System.out.println("Couleur : Noir");
+		System.out.println("Largeur : "+l_max);
+		System.out.println("Hauteur : "+h);
 	    }
-
-	    if (l_max = l) {
-		
+     
 	}
-
-
-    }
     
-    public static void analyse_ppm () {
+    
+    } 
+    
+    public void analyse_ppm () {
 	/* Procedure qui analyse l'image .ppm 
 	 * Affiche les résultats de l'analyse
 	 */ 
