a81c73b3b4407d689a18a6e838ee31ea786150f5
AntoineS73 <antoinesteyer@gmail.com>
2015-04-01 10:59:09 +0200

    Ajout de la reconnaissance du cercle

--- Matrice.java
+++ Matrice.java
@@ -42,7 +42,7 @@ public class Matrice {
 	this.image = new int[this.hauteur][this.largeur];
 	for (int i=0 ; i<this.hauteur ; i++) {
 	    for (int j=0 ; j<this.largeur ; j++) {
-		this.image[i][j] = Integer.parseInt(caracteres[i][j]);
+		this.image[i][j] = Integer.parseInt(caracteres[j][i]);
             }
         }
 	
@@ -74,65 +74,142 @@ public class Matrice {
 	    
 
 	    
-	    int l = 0; // Compteur de la largeur de la figure sur une ligne
-	    int l_max = 0; // Valeur maximale de la largeur de la figure
-	    int h = 0; // hauteur de la figure
+	    int h = 0; // Compteur de la largeur de la figure sur une colonne
+	    int h_max = 0; // Valeur maximale de la largeur de la figure
+	    int l = 0; // largeur de la figure
+
 	    // coordonnées du point d'origine de la figure
-	    int x=0 ;
-	    int y=0 ; 
+	    int x1=0 ;
+	    int y1=0 ;
 	    
+	    // coordonnées du deuxième point de la base (triangle)
+	    int x2=0;
+	    int y2=0;
+	    
+	    // coordonnées du sommet du triangle
+	    int x3=0;
+	    int y3=0;
+	    	    	    
 	    // Booleens mis à jour si on reconnait la figure
 	    boolean estRectangle = false;
 	    boolean estCercle = false;
 	    boolean estTriangle = false;
 
+	    // ypixel sert à dire si on a commencé à parcourir la figure
+	    boolean ypixel = false;
+
+	    // sup sert à savoir si on avait un moment donné h>h_max (élimine le rectangle)
+	    boolean sup = false;
+	    
+	    boolean onaBase = false;
+	    
 	    // On parcourt notre image
-	    for (int i=0; i<this.hauteur ; i++) {
-       
-		for (int j=0; j<this.largeur ; j++) {
-    		    if (this.image[i][j] == 0) {
+	    
+	    for (int j=0; j<this.largeur ; j++) {
+		
+		for (int i=0; i<this.hauteur ; i++) {
+		    
+
+		    
+		    if (this.image[i][j] == 0) {
 			// Si on a un pixel noir
-			l++ ;
-    			if (x==0 && y==0) {
-    			    // on regarde le premier point de la figure
-    			    x = j;
-    			    y = i;
-    			}
-			if (j == this.largeur-1 || this.image[i][j+1] == 1) {
-			    // Si on arrive à la fin de la figure
-			    // sur la ligne courante
-			    // on incrémente la hauteur de cette figure
-			    h++;
+			h++ ;
+			ypixel = true;
+			if (x1==0 && y1==0) {
+			    // on regarde le premier point de la figure
+			    x1 = j;
+			    y1 = i;
+			    if (this.image[i+1][j+1] == 0 && onaBase) {
+				onaBase = true;
+			    }
+			    else{
+				onaBase=false;
+			    }
 			}
-		    }   
-        	}
-		if (l == l_max) {
-		    // Si on a deux lignes de même longueur qui se suivent
+			if (this.image[i][j+1] == 1) {
+			    
+			    x3 = j;
+			    y3 = i;
+			}
+			if (this.image[i+1][j] == 1){
+			    // Si on arrive à la fin de la figure
+			    // sur la colonne courante
+			    // on incrémente la largeur de cette figure
+			    l++;
+			}
+			if (h == 1 && this.image[i+1][j] == 1) {
+			    // Si on arrive à la fin d'une colonne et qu'on avait qu'un pixel de couleur
+			    // cela signifie qu'on a trouvé le dernier point de la figure
+			    // (troisième sommet du triangle)
+			    
+			    x2 = j;
+			    y2 = i;
+			}
+			
+		    }
+		    
+		    
+		    
+		}
+		if (h == h_max && ypixel && ! sup && ! estTriangle) {
+		    // Si on a deux colonnes de même longueur qui se suivent
 		    // on a un rectangle
 		    estRectangle = true;
+		    		}
+		if (h > h_max) {
+		    // On met à jour la hauteur maximale
+		    h_max = h ;
+		    if (l>1) {
+			sup = true;
+		    }
 		}
-		if (l > l_max) {
-		    // On met à jour la ligne maximale
-    		    l_max = l ;
-    		}
-	       
-    		// On arrive à une nouvelle ligne
-    		l = 0 ;
-    	    }
+		if (sup && h < h_max && ! estRectangle && onaBase) {
+		    // Si la hauteur courante est inférieur à la hauteur max
+		    // on a un triangle
+		    estTriangle = true;
+		}
+		
+		
+    		// On arrive à une nouvelle colonne
+    		h = 0 ;
+	    }
+    	    
 	    
 	    if (estRectangle) {
 		// Si on a un rectangle
-		if (l_max == h) {
+		if (h_max == l) {
 		    // Le cas d'un carré
 		    System.out.println("La figure est un carré");
 		}
 		else {
 		    System.out.println("La figure est un rectangle");
 		}
-		System.out.println("Point origine : ("+x+","+y+")");
+		System.out.println("Point origine : ("+x1+","+y1+")");
 		System.out.println("Couleur : Noir");
-		System.out.println("Largeur : "+l_max);
-		System.out.println("Hauteur : "+h);
+		System.out.println("Largeur : "+l);
+		System.out.println("Hauteur : "+h_max);
+	    }
+	    else{
+
+		if (estTriangle) {
+		    // Si on a un triangle
+		    System.out.println("La figure est un triangle");
+		    System.out.println("Sommet 1 : ("+x1+","+y1+")");
+		    System.out.println("Sommet 2 : ("+x2+","+y2+")");
+		    System.out.println("Sommet 3 : ("+x3+","+y3+")");
+		    System.out.println("Couleur : Noir");
+		    System.out.println("Longueur de la base : "+h_max);
+		    System.out.println("Hauteur : "+l);
+		}
+		else{
+		    // Si on a un cercle
+		    System.out.println("La figure est un cercle");
+		    System.out.println("Couleur : Noir");
+		    int centrex = x1+l/2;
+		    System.out.println("Centre : ("+centrex+","+y1+")");
+		    int rayon = h_max/2;
+		    System.out.println("Rayon : "+rayon);
+		}
 	    }
      
 	}
